9d9aaf1d9bb24164df11d3f09ddfa11c704c9843
Ashish Krishnan <ashishkrish09@gmail.com>
2017-03-14 23:40:29 +0530

    Added Mixpanel Integration

--- app/src/main/java/org/askdn/popularmovies/MainActivity.java
+++ app/src/main/java/org/askdn/popularmovies/MainActivity.java
@@ -3,19 +3,41 @@ package org.askdn.popularmovies;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
+
+import com.mixpanel.android.mpmetrics.MixpanelAPI;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 /*The Launcher Activity*/
 public class MainActivity extends AppCompatActivity {
 
+    String MIXPANEL_TOKEN = "bdf2909825dd07af271f75c2ad560407";
     boolean mDualPane;
     public final String DFTAG = "detail_fragment";
+    private MixpanelAPI mixpanel;
+
+{
+        mixpanel =
+                MixpanelAPI.getInstance(getApplicationContext(), MIXPANEL_TOKEN);
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        try {
+            JSONObject props = new JSONObject();
+            props.put("Gender", "Female");
+            props.put("Logged in", false);
+            mixpanel.track("MainActivity - onCreate called", props);
+        } catch (JSONException e) {
+            Log.e("MYAPP", "Unable to add properties to JSONObject", e);
+        }
 
         if(findViewById(R.id.movie_detail_container)!=null) {
             mDualPane = true;
@@ -24,5 +46,12 @@ public class MainActivity extends AppCompatActivity {
         }
         else
             mDualPane = false;
+    }
+
+    @Override
+    protected void onDestroy() {
+        mixpanel.flush();
+        super.onDestroy();
+
     }
 }
